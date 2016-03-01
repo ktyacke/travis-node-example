951db4458122e9b409ee456083879f3a4275f7a1
Kyle Tyacke <ktyacke@gmail.com>
2016-03-01 12:11:58 -0500

    Flushing out hello.js with some additional greetings.

--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,19 @@
-module.exports.sayHello = function(name){
-  return "Hello " + name + ".";
-}
+module.exports.sayHey = function(name){
+  return "Hey " + name + ".";
+};
+
+module.exports.sayHowdy = function(name){
+  return "Howdy " + name + ".";
+};
+
+module.exports.sayHola = function(name){
+  return "Hola " + name + ".";
+};
+
+module.exports.sayHowzit = function(name){
+  return "Howzit " + name + ".";
+};
+
+module.exports.saySup = function(name){
+  return "Sup " + name + ".";
+};
