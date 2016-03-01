38bd43cf5de887706c17dea0ef30041f38e64f47
Kyle Tyacke <ktyacke@gmail.com>
2016-03-01 12:11:15 -0500

    Adding content that should force a conflict with hello.js

--- src/hello.js
+++ src/hello.js
@@ -1,3 +1,20 @@
-module.exports.sayHello = function(name){
-  return "Hello " + name + ".";
-}
+module.exports.sayHowdy = function(name)
+{
+  return "Howdy " + name + "!";
+};
+module.exports.sayHey = function(name)
+{
+  return "Hey " + name + "!";
+};
+module.exports.sayHola = function(name)
+{
+  return "Hola " + name + "!";
+};
+module.exports.saySup = function(name)
+{
+  return "Sup " + name + "!";
+};
+module.exports.sayHowzit = function(name)
+{
+  return "Howzit " + name + "!";
+};
