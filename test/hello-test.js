bba76e11f596c72b59825741e87978fb47e367f8
Kyle Tyacke <ktyacke@gmail.com>
2016-03-31 16:21:34 -0400

    Add a breaking change to hello_test to show example of Travis failure.

--- test/hello-test.js
+++ test/hello-test.js
@@ -6,6 +6,6 @@ var hello = require('../src/hello');
 describe('hello', function() {
   it("Returns a string of hello plus the name that was passed.", function() {
     var output = hello.sayHello("Dave");
-    assert.equal(output, "Hello Dave.");
+    assert.equal(output, "Hello Johnny.");
   });
 });
