e63284abae5dde78128e1cb1ac47f9d3c8dd20cb
Kyle Tyacke <ktyacke@gmail.com>
2016-04-06 17:00:52 -0400

    Removing erroneous methods

--- src/hello.js
+++ src/hello.js
@@ -1,19 +1,3 @@
 module.exports.sayHey = function(name){
   return "Hey " + name + ".";
 };
-
-module.exports.sayHowdy = function(name){
-  return "Howdy " + name + ".";
-};
-
-module.exports.sayHola = function(name){
-  return "Hola " + name + ".";
-};
-
-module.exports.sayHowzit = function(name){
-  return "Howzit " + name + ".";
-};
-
-module.exports.saySup = function(name){
-  return "Sup " + name + ".";
-};
